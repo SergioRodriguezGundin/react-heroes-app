ccac00c9f4797564d546aa4ca3329d498535989a
Sergio Dixper <sergio.gundin.rodriguez@gmail.com>
2021-12-29 11:19:49 +0100

    first commit

---
+++ src/HeroesApp.js
@@ -0,0 +1,10 @@
+export const HeroesApp = () => {
+  return (
+    <div className="container m-40 mx-auto">
+      <h1 className="text-2xl font-bold text-cyan-800">Heroes App!</h1>
+      <button className="px-6 py-2 text-sm text-white rounded shadow-xl bg-emerald-500">
+        success
+      </button>
+    </div>
+  );
+};
